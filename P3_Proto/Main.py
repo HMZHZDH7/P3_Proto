4d881be72720f4ac1378c840f22f12da631f409e
HMZHZDH7 <56533642+HMZHZDH7@users.noreply.github.com>
2019-10-14 14:01:43 +0200

    salty update no. 68

--- P3_Proto/Main.py
+++ P3_Proto/Main.py
@@ -8,31 +8,11 @@ while True:
     ret, frame = cap.read()
 
     IP = imageProcessor(frame)
-    mask = IP.image_to_binary(frame)
+    mask = IP.create_mask(frame)
     mask_Closed = IP.reduce_noise(mask)
+    bloby_image = IP.detect_blobs(mask_Closed)
 
-    # Setup SimpleBlobDetector parameters.
-    params = cv2.SimpleBlobDetector_Params()
-
-    params.filterByArea = True
-    params.minArea = 200
-    params.filterByColor = True
-    params.blobColor = 255
-    params.filterByCircularity = False
-    params.filterByConvexity = False
-    params.filterByInertia = False
-
-    # Create a detector with the parameters
-    ver = cv2.__version__.split('.')
-    if int(ver[0]) < 3:
-        detector = cv2.SimpleBlobDetector(params)
-    else:
-        detector = cv2.SimpleBlobDetector_create(params)
-
-    Blobs = detector.detect(mask_Closed)
-    print(Blobs)
-
-    pts = cv2.KeyPoint_convert(Blobs)
+    pts = cv2.KeyPoint_convert(bloby_image)
 
     if len(pts) == 2:
         if pts[0, 0] > pts[1, 0]:
@@ -42,7 +22,7 @@ while True:
             left_hand = pts[1, 0]
             right_hand = pts[0, 0]
 
-    im_with_keypoints = cv2.drawKeypoints(mask_Closed, Blobs, np.array([]), (0, 0, 255),
+    im_with_keypoints = cv2.drawKeypoints(mask_Closed, bloby_image, np.array([]), (0, 0, 255),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     cv2.imshow('frame', frame)
--- P3_Proto/image_Processor.py
+++ P3_Proto/image_Processor.py
@@ -12,14 +12,35 @@ class imageProcessor:
     def __init__(self, frame):
         self.binary_Image = frame
 
-    def image_to_binary(self, image):
+    def create_mask(self, image):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, (50, 25, 25), (100, 200, 200))
         return mask
 
     def reduce_noise(self, image):
         kernel = np.ones((5, 5), np.uint8)
-
         mask_opened = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
         mask_closed = cv2.morphologyEx(mask_opened, cv2.MORPH_CLOSE, kernel)
         return mask_closed
+
+    def detect_blobs(self, image):
+        # Setup SimpleBlobDetector parameters.
+        params = cv2.SimpleBlobDetector_Params()
+
+        params.filterByArea = True
+        params.minArea = 200
+        params.filterByColor = True
+        params.blobColor = 255
+        params.filterByCircularity = False
+        params.filterByConvexity = False
+        params.filterByInertia = False
+
+        # Create a detector with the parameters
+        ver = cv2.__version__.split('.')
+        if int(ver[0]) < 3:
+            detector = cv2.SimpleBlobDetector(params)
+        else:
+            detector = cv2.SimpleBlobDetector_create(params)
+
+        blobs = detector.detect(image)
+        return blobs
