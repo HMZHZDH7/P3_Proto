22b6d4075c9b0ca6b6cad8d750c872f487c2cbf5
HMZHZDH7 <56533642+HMZHZDH7@users.noreply.github.com>
2019-10-14 14:13:41 +0200

    salty update no. 68

--- P3_Proto/Main.py
+++ P3_Proto/Main.py
@@ -11,16 +11,7 @@ while True:
     mask = IP.create_mask(frame)
     mask_Closed = IP.reduce_noise(mask)
     bloby_image = IP.detect_blobs(mask_Closed)
-
-    pts = cv2.KeyPoint_convert(bloby_image)
-
-    if len(pts) == 2:
-        if pts[0, 0] > pts[1, 0]:
-            left_hand = pts[0, 0]
-            right_hand = pts[1, 0]
-        else:
-            left_hand = pts[1, 0]
-            right_hand = pts[0, 0]
+    IP.locate_hands(bloby_image)
 
     im_with_keypoints = cv2.drawKeypoints(mask_Closed, bloby_image, np.array([]), (0, 0, 255),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
--- P3_Proto/image_Processor.py
+++ P3_Proto/image_Processor.py
@@ -10,6 +10,10 @@ class imageProcessor:
     binary_Image = None
 
     def __init__(self, frame):
+        power = 0.0
+        distance = 0.0
+        pos_left_hand = 0.0
+        pos_right_hand = 0.0
         self.binary_Image = frame
 
     def create_mask(self, image):
@@ -44,3 +48,14 @@ class imageProcessor:
 
         blobs = detector.detect(image)
         return blobs
+
+    def locate_hands(self, image):
+        pts = cv2.KeyPoint_convert(image)
+
+        if len(pts) == 2:
+            if pts[0, 0] > pts[1, 0]:
+                self.pos_left_hand = pts[0, 0]
+                self.pos_right_hand = pts[1, 0]
+            else:
+                self.pos_left_hand = pts[1, 0]
+                self.pos_right_hand = pts[0, 0]
